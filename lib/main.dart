6602369935c9d902980c3d345507b9c3ef9224f1
ayuariesta <=>
2022-09-09 07:48:18 +0700

    Menerapkan Load Progress Event

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'src/web_view_stack.dart';
 
 void main() {
   runApp(
@@ -19,22 +18,12 @@ class WebViewApp extends StatefulWidget {
 
 class _WebViewAppState extends State<WebViewApp> {
   @override
-  void initState() {
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter WebView'),
       ),
-      body: const WebView(
-        initialUrl: 'https://flutter.dev',
-      ),
+      body: const WebViewStack(),
     );
   }
 }
